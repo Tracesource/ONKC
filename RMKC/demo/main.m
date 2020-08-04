3913d0f6c5418f554cde8c8e5443f6d4c672693c
tracesource <751871305@qq.com>
2020-08-04 08:20:43 +0800

    update .gitigore

--- RMKC/demo/main.m
+++
@@ -1,7 +0,0 @@
-
-ds={'YALE_165n_1024d_15c_zscore_uni'};
-addpath(fullfile(pwd, '..', 'lib'));
-for id = 1:length(ds)
-    dataset = ['..\data\', ds{id}]
-    runExp1(dataset, buildParamKernel({'Linear', 'Polynomial', 'Gaussian'}, {[],  [2, 4], sqrt([0.01,  0.1, 1, 10,  100] / sqrt(2))}, {'NCW-SCALE-SYM'}), 10,'NCW-SCALE-SYM');
-end
--- RMKC/lib/Kernel_NCW.m
+++
@@ -1,4 +0,0 @@
-function [K] = Kernel_NCW(K)
-w = sum(K, 2).^-0.5;
-K = bsxfun(@times, K, w);
-K = bsxfun(@times, K, w');
